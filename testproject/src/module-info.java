a1f12f623b0a733f7bb6f8f56a3d28a3fa096037
kashaboina <kashaboina@LAPTOP-QM5F642G>
2022-12-23 14:47:31 +0530

    vinisha

---
+++ testproject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author kashaboina
+ *
+ */
+module testproject {
+}
---
+++ testproject/src/testproject/drivercls.java
@@ -0,0 +1,12 @@
+package testproject;
+
+public class drivercls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("{hello task");
+
+	}
+
+}
